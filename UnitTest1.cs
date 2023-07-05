e8d21969654c7c2da309afca22615cb62005cd4e
Jay Chotaliya <jay.chotaliya@ajmerainfotech.com>
2023-07-05 16:57:39 +0530

    Setup mobile test project

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StykuMobileTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
