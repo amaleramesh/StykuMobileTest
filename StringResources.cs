21cba7dacae916a8dffe3e75c65ddf7f6bddea79
amaleramesh <137982103+amaleramesh@users.noreply.github.com>
2023-08-23 10:47:27 +0530

    Update test cases of profile information (#3)
    * Updated test cases of profile information
    
    * Updated test cases of Flagged Scans

--- StringResources.cs
+++ StringResources.cs
@@ -4,7 +4,7 @@
     {
         public const string APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub";
         public const string DEVICE_NAME = "Galaxy Nexus API 33";
-        public const string EMAIL_ID = "ramesh.amale+160@ajmerainfotech.com";
+        public const string EMAIL_ID = "ramesh.amale+283@ajmerainfotech.com";
         public const string PASSWORD = "Test@123";
         public const string INVALID_PASSWORD = "Test@1234";
         public const string INVALID_EMAIL_ID = "ramesh.amaleajmerainfotech.com"; 
